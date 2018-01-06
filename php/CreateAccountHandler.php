0514a81061ef4d81a37cb9b7f4b3fbd78c1d36ca
CharlieStewart <charliestewart@MBPs.home>
2018-01-06 23:20:05 +0000

    Fixed registering + added username regex validation

--- php/CreateAccountHandler.php
+++ php/CreateAccountHandler.php
@@ -9,10 +9,17 @@ if($recaptcha) {
   $username = $_POST["username"];
   $password = $_POST["password"];
   $c_password = $_POST["c_password"];
+  if($password != $c_password) {
+    performError("Passwords do not match!", "../create_account.php");
+    return;
+  } else if(preg_match('/[^a-z_\-0-9\S]/i', $username)) {
+    performError("Please only user alphanumerics and numbers in your username.", "../create_account.php");
+    return;
+  }
   $user = new UserHandler($username, $conn);
   $validation = $user->validate($password, $c_password);
   $exists = $user->user_exists();
-  if($exists) {
+  if($exists["response"]) {
     performError("Username already exists!", "../create_account.php");
   } else {
     if($validation["response"]) {
